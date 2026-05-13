78833354327ec4c03312df9ec2ad421d7a01ea40
tomzit <barisakib6@gmail.com>
2026-05-13 23:32:41 +0600

    setup riverpod providers and dependency injection

---
+++ lib/features/fitness/presentation/providers/fitness_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../data/datasources/fitness_local_datasource.dart';
+import '../../data/repositories/fitness_repository_impl.dart';
+import '../../domain/entities/fitness_activity_entity.dart';
+import '../../domain/usecases/add_activity_usecase.dart';
+import '../../domain/usecases/get_activities_usecase.dart';
+
+final localDataSourceProvider = Provider<FitnessLocalDataSource>(
+  (ref) => FitnessLocalDataSource(),
+);
+
+final repositoryProvider = Provider<FitnessRepositoryImpl>(
+  (ref) => FitnessRepositoryImpl(ref.read(localDataSourceProvider)),
+);
+
+final addActivityUseCaseProvider = Provider<AddActivityUseCase>(
+  (ref) => AddActivityUseCase(ref.read(repositoryProvider)),
+);
+
+final getActivitiesUseCaseProvider = Provider<GetActivitiesUseCase>(
+  (ref) => GetActivitiesUseCase(ref.read(repositoryProvider)),
+);
+
+final activitiesProvider = FutureProvider<List<FitnessActivityEntity>>((
+  ref,
+) async {
+  return ref.read(getActivitiesUseCaseProvider).call();
+});
