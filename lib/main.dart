df1f7b59997cd2bc465ec7dcd00facd549d60c4d
tomzit <barisakib6@gmail.com>
2026-05-13 23:12:34 +0600

    updated main file after database connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'core/services/hive_service.dart';
 import 'core/theme/app_theme.dart';
 import 'features/fitness/presentation/pages/dashboard_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await HiveService.init();
+
   runApp(const ProviderScope(child: FitnessApp()));
 }
 
@@ -15,11 +20,8 @@ class FitnessApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
       title: 'Fitness Tracker',
-
       theme: AppTheme.lightTheme,
-
       home: const DashboardPage(),
     );
   }
