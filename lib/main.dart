b94fb9334bd68bc9fa5bd122a8943d4d2ab70241
tomzit <barisakib6@gmail.com>
2026-05-13 23:45:36 +0600

    updated analytics screen with live chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
+import 'package:fitness_tracker_app/features/fitness/presentation/pages/main_navigation_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'core/services/hive_service.dart';
 import 'core/theme/app_theme.dart';
-import 'features/fitness/presentation/pages/dashboard_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,7 +22,7 @@ class FitnessApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Fitness Tracker',
       theme: AppTheme.lightTheme,
-      home: const DashboardPage(),
+      home: const MainNavigationPage(),
     );
   }
 }
