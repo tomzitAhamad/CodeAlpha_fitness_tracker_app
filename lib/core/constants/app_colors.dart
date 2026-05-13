f765ad5c62857140df103b4b0dc2c665a0f157f5
tomzit <barisakib6@gmail.com>
2026-05-13 22:35:18 +0600

    added app colors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF6C63FF);
+
+  static const secondary = Color(0xFF8E85FF);
+
+  static const background = Color(0xFFF7F8FA);
+
+  static const white = Colors.white;
+
+  static const black = Colors.black;
+
+  static const grey = Colors.grey;
+}
